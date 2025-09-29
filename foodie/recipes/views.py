7ad2212c50da7f4a565e61fb3177f083eed074f2
Tijo-11 <tijot111@gmail.com>
2025-09-29 17:33:19 +0530

    Edit and Delete option successful

--- foodie/recipes/views.py
+++ foodie/recipes/views.py
@@ -3,6 +3,8 @@ from .models import Recipe  # import the Recipe model
 from comments.forms import CommentForm
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden, HttpResponseNotAllowed
+from foodie_app.forms import RecipeForm
 
 def recipes_view(request):
     recipes = Recipe.objects.all()  # fetch all recipes
@@ -97,5 +99,41 @@ def favorite_recipes(request):
     return render(request, "recipes/favorites.html", context)
 
 
+@login_required
+def delete_recipe(request, recipe_id):
+    # Get the recipe to delete
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    
+    # Check if the user is allowed to delete
+    if not (request.user == recipe.user or request.user.is_superuser):
+        return HttpResponseForbidden()  # 403 if not owner/admin
+
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('recipes:recipes')  # Redirect to recipe index
+
+    # GET request: show confirmation page
+    context = {'recipe': recipe}
+    return render(request, 'recipes/recipe_confirmation_delete.html', context)
+
+@login_required
+def edit_recipe(request, recipe_id):
+    # Get the recipe object
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+
+    # Check if user is authorized
+    if not (request.user == recipe.user or request.user.is_superuser):
+        return HttpResponseNotAllowed()  # Could also use HttpResponseForbidden()
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipes:recipe_detail', recipe_id=recipe.id)
+    else:
+        form = RecipeForm(instance=recipe)  # Prepopulate form with existing data
+
+    context = {'form': form, 'recipe': recipe}
+    return render(request, 'recipes/recipe_form.html', context)
 
     
